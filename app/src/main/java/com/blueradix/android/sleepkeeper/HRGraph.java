26b3b15add4c7aa4d753e092d8d39e68860283a9
Shiori <shiori0921syd@gmail.com>
2023-03-11 18:02:05 +1100

    Develop Heart rate file part

--- app/src/main/java/com/blueradix/android/sleepkeeper/HRGraph.java
+++ app/src/main/java/com/blueradix/android/sleepkeeper/HRGraph.java
@@ -1,4 +1,37 @@
 package com.blueradix.android.sleepkeeper;
 
-public class HRGraph {
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.androidplot.xy.XYPlot;
+import com.polar.sdk.api.PolarBleApi;
+
+public class HRGraph extends AppCompatActivity {
+    private PolarBleApi api;
+  //  private HrAndRrPlotter plotter;
+    private TextView textViewAve;
+    private TextView textViewMax;
+    private TextView textViewMin;
+    private TextView textViewId;
+    private XYPlot plot;
+    private static final String TAG = "HRGraph";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hr_graph);
+        String DEVICE_ID = this.getIntent().getStringExtra("id");
+        textViewId = findViewById(R.id.textViewID);
+        textViewAve = findViewById(R.id.textViewAve);
+        textViewMax = findViewById(R.id.textViewMax);
+        textViewMin = findViewById(R.id.textViewMin);
+
+    }
+
+
 }
+
+
