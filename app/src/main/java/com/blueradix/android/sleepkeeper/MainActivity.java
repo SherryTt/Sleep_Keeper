4e654b9d00d5ebd1830eca216a671220d372fac8
Shiori <shiori0921syd@gmail.com>
2023-03-11 17:14:26 +1100

    Fix alert dialog

--- app/src/main/java/com/blueradix/android/sleepkeeper/MainActivity.java
+++ app/src/main/java/com/blueradix/android/sleepkeeper/MainActivity.java
@@ -1,8 +1,8 @@
 package com.blueradix.android.sleepkeeper;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
@@ -12,18 +12,20 @@ import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.InputType;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import kotlin.jvm.internal.Intrinsics;
+
 public class MainActivity extends AppCompatActivity {
 
     private String TAG = "Polar_MainActivity";
     private String sharedPrefsKey = "polar_device_id";
     private String DEVICE_ID;
+    private ActivityResultLauncher bluetoothOnActivityResultLauncher;
     SharedPreferences sharedPreferences;
 
     @Override
@@ -34,27 +36,43 @@ public class MainActivity extends AppCompatActivity {
         checkBT();
     }
 
+
     public void onClickConnectHr(View view) {
         checkBT();
+        if (this.DEVICE_ID != null && !Intrinsics.areEqual(this.DEVICE_ID, "")) {
+            this.showToast("connect to device " + " " + this.DEVICE_ID);
+            Intent intent = new Intent((Context)this, HRGraph.class);
+            intent.putExtra("id", this.DEVICE_ID);
+            this.startActivity(intent);
+        } else {
+            SharedPreferences var10001 = this.sharedPreferences;
+            if (var10001 == null) {
+                Intrinsics.throwUninitializedPropertyAccessException("sharedPreferences");
+            }
+
+            this.DEVICE_ID = var10001.getString("polar_device_id", "");
+            this.showDialog(view);
+        }
+
+        /*
         DEVICE_ID = sharedPreferences.getString(sharedPrefsKey,"");
         Log.d(TAG,DEVICE_ID);
-        if(DEVICE_ID.equals("")){
+        if(DEVICE_ID.equals("") || DEVICE_ID.equals(null)){
             showDialog(view);
         } else {
-            Toast.makeText(this,getString(R.string.connecting) + " " + DEVICE_ID,Toast.LENGTH_SHORT).show();
+            showToast(this,getString(R.string.connecting) + " " + DEVICE_ID);
             Intent intent = new Intent(this, HRGraph.class);
             intent.putExtra("id", DEVICE_ID);
             startActivity(intent);
-        }
+        }*/
     }
 
     public void onClickChangeID(View view) {
-
         showDialog(view);
     }
 
     public void showDialog(View view){
-        AlertDialog.Builder dialog = new AlertDialog.Builder(this, R.style.PolarTheme);
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
         dialog.setTitle("Enter your Polar device's ID");
 
         View viewInflated = LayoutInflater.from(getApplicationContext()).inflate(R.layout.activity_select_ble,(ViewGroup) view.getRootView() , false);
@@ -84,8 +102,8 @@ public class MainActivity extends AppCompatActivity {
     public void checkBT(){
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter != null && !mBluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, 2);
+            Intent enableBtIntent = new Intent("android.bluetooth.adapter.action.REQUEST_ENABLE");
+            this.bluetoothOnActivityResultLauncher.launch(enableBtIntent);
         }
 
         //requestPermissions() method needs to be called when the build SDK version is 23 or above
@@ -94,4 +112,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void showToast(String message){
+        Toast toast = Toast.makeText(this.getApplicationContext(),message,Toast.LENGTH_SHORT);
+        toast.show();
+    }
 }
