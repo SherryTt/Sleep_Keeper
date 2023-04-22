1f7288cd4a742cc3cfbf95e5f24250c001058a94
Shiori <shiori0921syd@gmail.com>
2023-04-22 20:54:02 +1000

    Add splash screen

--- app/src/main/java/com/blueradix/android/sleepkeeper/SplashActivity.java
+++ app/src/main/java/com/blueradix/android/sleepkeeper/SplashActivity.java
@@ -13,32 +13,22 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
 
-
-        // on below line we are configuring our window to full screen
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_splash);
-
-        // on below line we are calling handler to run a task
-        // for specific time interval
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                // on below line we are
-                // creating a new intent
                 Intent i = new Intent(SplashActivity.this, SignUpActivity.class);
 
-                // on below line we are
-                // starting a new activity.
                 startActivity(i);
 
-                // on the below line we are finishing
-                // our current activity.
                 finish();
             }
-        }, 2000);
+        }, 3000);
 
     }
 }
