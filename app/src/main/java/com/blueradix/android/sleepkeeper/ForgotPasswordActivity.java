7e947513d48bae0a1d2aa6406421bef9dcd32de5
Shiori <shiori0921syd@gmail.com>
2023-04-25 13:45:42 +1000

    Change background and clean design

--- app/src/main/java/com/blueradix/android/sleepkeeper/ForgotPasswordActivity.java
+++ app/src/main/java/com/blueradix/android/sleepkeeper/ForgotPasswordActivity.java
@@ -4,9 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -30,6 +33,8 @@ public class ForgotPasswordActivity extends AppCompatActivity {
         registeredEmailInput = findViewById(R.id.txtResetEmailInput);
         resetPassBtn = findViewById(R.id.ResetBtn);
         mAuth = FirebaseAuth.getInstance();
+
+
 
         resetPassBtn.setOnClickListener(new View.OnClickListener() {
             @Override
